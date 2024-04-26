8ff72993c0db4ce2060f1c18fc45199e557d7cd2
Alveszx <adrian.j.santos@aluno.senai.br>
2024-04-26 09:53:05 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const dom = 1;
+const seg = 2;
+const ter = 3;
+const qua = 4;
+const qui = 5;
+const sex = 6;
+const sab = 7;
+
+let dia = 2;
+
+if ( dia == 1){
+    Document.write("domingo dia 'dormir' ");
+}else if (dia == 2){
+    document.write("segunda, dia de perna");
+}else if ( dia == 3){
+    document.write("terça, dia de peito e tricipes");
+}else if ( dia == 4){
+    document.write("quarta, dia de costa e biceps");
+}else if ( dia == 5){
+    document.write("quinta, dia de ombro e antebraço");
+}else if ( dia == 6){
+    document.write("sexta, dis de descanso");
+}else if ( dia ==7){
+    document.write("sabado, dia de fuder");
+}
